b577204ae2e3cdad889027b21f7f589b1f2c748e
DESKTOP-UGV6KDC\GarryTayler <superhack0811@outlook.com>
2022-01-25 19:42:17 +0900

    update ABYSSVesting
    update ABYSSVesting

--- contracts/ABYSSVesting.sol
+++ contracts/ABYSSVesting.sol
@@ -3,251 +3,144 @@ pragma solidity ^0.8.4;
 
 import "./dependencies/interface/IERC20.sol";
 import "./dependencies/libraries/SafeERC20.sol";
+import "./dependencies/Trustable.sol";
 import "./dependencies/ReentrancyGuard.sol";
-import "./dependencies/Ownable.sol";
 
-contract ABYSSVesting is Ownable, ReentrancyGuard {
+contract ABYSSVesting is Trustable, ReentrancyGuard {
     using SafeERC20 for IERC20;
 
-    struct VestingSchedule{
+    struct VestingSchedule {
         bool initialized;
         address  beneficiary;
-        uint256  cliff;
-        uint256  start;
-        uint256  duration;
-        uint256 slicePeriodSeconds;
-        bool  revocable;
         uint256 amountTotal;
-        uint256  released;
+        uint256[] durations;
+        uint256[] releaseAmounts;
         bool revoked;
+        bool released;
+        uint256 startIndex;
+        uint256 vestedEnd;
     }
 
-    // address of main token
-    IERC20 immutable private _token;
+    uint256 private start;
+    uint256 private decimals = 18;
+    IERC20 immutable private token;
 
-    bytes32[] private vestingSchedulesIds;
-    mapping(bytes32 => VestingSchedule) private vestingSchedules;
+    VestingSchedule[] private vestingSchedules;
     uint256 private vestingSchedulesTotalAmount;
-    mapping(address => uint256) private holdersVestingCount;
 
-    event Released(uint256 amount);
-    event Revoked();
-
-    modifier onlyIfVestingScheduleExists(bytes32 vestingScheduleId) {
-        require(vestingSchedules[vestingScheduleId].initialized == true);
-        _;
-    }
-
-    modifier onlyIfVestingScheduleNotRevoked(bytes32 vestingScheduleId) {
-        require(vestingSchedules[vestingScheduleId].initialized == true);
-        require(vestingSchedules[vestingScheduleId].revoked == false);
+    modifier onlyIfVestingScheduleValid(uint256 vestingScheduleId) {
+        require(vestingSchedules[vestingScheduleId].initialized == true
+             && !vestingSchedules[vestingScheduleId].revoked
+             && !vestingSchedules[vestingScheduleId].released);
         _;
     }
 
     constructor(address token_) {
         require(token_ != address(0x0));
-        _token = IERC20(token_);
+        token = IERC20(token_);
     }
 
-    receive() external payable {}
+    function createVestingSchedule(
+        address _beneficiary,
+        uint256 _vestAmount,
+        uint256[] memory _durations,
+        uint256[] memory _releaseAmounts
+    ) public isTrusted {
+        require(_beneficiary != address(0), "beneficiary is the zero address!");
+        require(this.getWithdrawableAmount() >= _vestAmount, 
+        "Cannot create vesting schedule because not sufficient tokens");
+        require(_durations.length == _releaseAmounts.length,
+        "length miss match");
 
-    fallback() external payable {}
+        uint256 _toReleaseSum;
+        for(uint i = 0; i < _releaseAmounts.length; i ++) {
+            _toReleaseSum = _toReleaseSum + _releaseAmounts[i];
+        }
+        require(_vestAmount == _toReleaseSum, "Vest amount not match");
 
-    function getVestingSchedulesCountByBeneficiary(address _beneficiary)
-    external
-    view
-    returns(uint256){
-        return holdersVestingCount[_beneficiary];
+        vestingSchedules.push(VestingSchedule(true, _beneficiary, _vestAmount, _durations, _releaseAmounts, false, false, 0, start));
+        vestingSchedulesTotalAmount = vestingSchedulesTotalAmount + _vestAmount;
     }
 
-    function getVestingIdAtIndex(uint256 index)
-    external
-    view
-    returns(bytes32){
-        require(index < getVestingSchedulesCount(), "TokenVesting: index out of bounds");
-        return vestingSchedulesIds[index];
+    function release()
+    public
+    nonReentrant isTrusted {
+        for(uint i = 0; i < vestingSchedules.length; i ++) {
+            
+            VestingSchedule storage vestingSchedule = vestingSchedules[i];
+
+            if(vestingSchedule.released || vestingSchedule.revoked)
+                continue;
+
+            (uint _releaseAmount, uint _startIndex) = _computeReleasableAmount(vestingSchedule);
+
+            if(_releaseAmount != 0) {
+                vestingSchedule.amountTotal = vestingSchedule.amountTotal - _releaseAmount;
+                if(vestingSchedule.amountTotal == 0) {
+                    vestingSchedule.released = true;
+                }
+                vestingSchedule.startIndex = _startIndex;
+                vestingSchedulesTotalAmount = vestingSchedulesTotalAmount - _releaseAmount;
+                token.safeTransfer(vestingSchedule.beneficiary, _releaseAmount);
+            }
+        }
     }
 
-    function getVestingScheduleByAddressAndIndex(address holder, uint256 index)
-    external
-    view
-    returns(VestingSchedule memory){
-        return getVestingSchedule(computeVestingScheduleIdForAddressAndIndex(holder, index));
+    function revoke(uint256 vestingScheduleId) public isTrusted onlyIfVestingScheduleValid(vestingScheduleId) {
+        VestingSchedule storage vestingSchedule = vestingSchedules[vestingScheduleId];
+        (uint _releaseAmount,) = _computeReleasableAmount(vestingSchedule);
+        if(_releaseAmount != 0) {
+            token.safeTransfer(vestingSchedule.beneficiary, _releaseAmount);
+        }
+        vestingSchedulesTotalAmount = vestingSchedulesTotalAmount - vestingSchedule.amountTotal;
+        vestingSchedule.revoked = true;
+    }
+
+    function getWithdrawableAmount()
+    public 
+    view 
+    returns(uint256) {
+        return token.balanceOf(address(this)) - vestingSchedulesTotalAmount;
+    }
+
+    function getCurrentTime() 
+        internal
+        virtual
+        view
+        returns(uint256) {
+            return block.timestamp;
+    }
+
+    function getToken()
+        external 
+        view
+        returns(address) {
+        return address(token);
     }
 
     function getVestingSchedulesTotalAmount()
     external
     view
-    returns(uint256){
+    returns(uint256) {
         return vestingSchedulesTotalAmount;
-    }
-
-    function getToken()
-    external
-    view
-    returns(address){
-        return address(_token);
-    }
-
-    function createVestingSchedule(
-        address _beneficiary,
-        uint256 _start,
-        uint256 _cliff,
-        uint256 _duration,
-        uint256 _slicePeriodSeconds,
-        bool _revocable,
-        uint256 _amount
-    )
-        public
-        onlyOwner{
-        require(
-            this.getWithdrawableAmount() >= _amount,
-            "TokenVesting: cannot create vesting schedule because not sufficient tokens"
-        );
-        require(_duration > 0, "TokenVesting: duration must be > 0");
-        require(_amount > 0, "TokenVesting: amount must be > 0");
-        require(_slicePeriodSeconds >= 1, "TokenVesting: slicePeriodSeconds must be >= 1");
-        bytes32 vestingScheduleId = this.computeNextVestingScheduleIdForHolder(_beneficiary);
-        uint256 cliff = _start + _cliff;
-        vestingSchedules[vestingScheduleId] = VestingSchedule(
-            true,
-            _beneficiary,
-            cliff,
-            _start,
-            _duration,
-            _slicePeriodSeconds,
-            _revocable,
-            _amount,
-            0,
-            false
-        );
-        vestingSchedulesTotalAmount = vestingSchedulesTotalAmount + _amount;
-        vestingSchedulesIds.push(vestingScheduleId);
-        uint256 currentVestingCount = holdersVestingCount[_beneficiary];
-        holdersVestingCount[_beneficiary] = currentVestingCount + 1;
-    }
-
-    function revoke(bytes32 vestingScheduleId)
-        public
-        onlyOwner
-        onlyIfVestingScheduleNotRevoked(vestingScheduleId){
-        VestingSchedule storage vestingSchedule = vestingSchedules[vestingScheduleId];
-        require(vestingSchedule.revocable == true, "TokenVesting: vesting is not revocable");
-        uint256 vestedAmount = _computeReleasableAmount(vestingSchedule);
-        if(vestedAmount > 0){
-            release(vestingScheduleId, vestedAmount);
-        }
-        uint256 unreleased = vestingSchedule.amountTotal - vestingSchedule.released;
-        vestingSchedulesTotalAmount = vestingSchedulesTotalAmount - unreleased;
-        vestingSchedule.revoked = true;
-    }
-
-    function withdraw(uint256 amount)
-        public
-        nonReentrant
-        onlyOwner{
-        require(this.getWithdrawableAmount() >= amount, "TokenVesting: not enough withdrawable funds");
-        _token.safeTransfer(owner(), amount);
-    }
-
-    function release(
-        bytes32 vestingScheduleId,
-        uint256 amount
-    )
-        public
-        nonReentrant
-        onlyIfVestingScheduleNotRevoked(vestingScheduleId){
-        VestingSchedule storage vestingSchedule = vestingSchedules[vestingScheduleId];
-        bool isBeneficiary = msg.sender == vestingSchedule.beneficiary;
-        bool isOwner = msg.sender == owner();
-        require(
-            isBeneficiary || isOwner,
-            "TokenVesting: only beneficiary and owner can release vested tokens"
-        );
-        uint256 vestedAmount = _computeReleasableAmount(vestingSchedule);
-        require(vestedAmount >= amount, "TokenVesting: cannot release tokens, not enough vested tokens");
-        vestingSchedule.released = vestingSchedule.released + amount;
-        address payable beneficiaryPayable = payable(vestingSchedule.beneficiary);
-        vestingSchedulesTotalAmount = vestingSchedulesTotalAmount - amount;
-        _token.safeTransfer(beneficiaryPayable, amount);
-    }
-
-    function getVestingSchedulesCount()
-        public
-        view
-        returns(uint256){
-        return vestingSchedulesIds.length;
-    }
-
-    function computeReleasableAmount(bytes32 vestingScheduleId)
-        public
-        onlyIfVestingScheduleNotRevoked(vestingScheduleId)
-        view
-        returns(uint256){
-        VestingSchedule storage vestingSchedule = vestingSchedules[vestingScheduleId];
-        return _computeReleasableAmount(vestingSchedule);
-    }
-
-    function getVestingSchedule(bytes32 vestingScheduleId)
-        public
-        view
-        returns(VestingSchedule memory){
-        return vestingSchedules[vestingScheduleId];
-    }
-
-    function getWithdrawableAmount()
-        public
-        view
-        returns(uint256){
-        return _token.balanceOf(address(this)) - vestingSchedulesTotalAmount;
-    }
-
-    function computeNextVestingScheduleIdForHolder(address holder)
-        public
-        view
-        returns(bytes32){
-        return computeVestingScheduleIdForAddressAndIndex(holder, holdersVestingCount[holder]);
-    }
-
-    function getLastVestingScheduleForHolder(address holder)
-        public
-        view
-        returns(VestingSchedule memory){
-        return vestingSchedules[computeVestingScheduleIdForAddressAndIndex(holder, holdersVestingCount[holder] - 1)];
-    }
-
-    function computeVestingScheduleIdForAddressAndIndex(address holder, uint256 index)
-        public
-        pure
-        returns(bytes32){
-        return keccak256(abi.encodePacked(holder, index));
     }
 
     function _computeReleasableAmount(VestingSchedule memory vestingSchedule)
     internal
     view
-    returns(uint256){
-        uint256 currentTime = getCurrentTime();
-        if ((currentTime < vestingSchedule.cliff) || vestingSchedule.revoked == true) {
-            return 0;
-        } else if (currentTime >= vestingSchedule.start + vestingSchedule.duration) {
-            return vestingSchedule.amountTotal - vestingSchedule.released;
-        } else {
-            uint256 timeFromStart = currentTime - vestingSchedule.start;
-            uint secondsPerSlice = vestingSchedule.slicePeriodSeconds;
-            uint256 vestedSlicePeriods = timeFromStart / secondsPerSlice;
-            uint256 vestedSeconds = vestedSlicePeriods * secondsPerSlice;
-            uint256 vestedAmount = vestingSchedule.amountTotal * vestedSeconds / vestingSchedule.duration;
-            vestedAmount = vestedAmount - vestingSchedule.released;
-            return vestedAmount;
+    returns(uint256, uint256) {
+        uint _releaseAmount;
+        uint _startIndex;
+        for(uint i = vestingSchedule.startIndex; i < vestingSchedule.durations.length; i ++) {
+            if(getCurrentTime() >= vestingSchedule.durations[i] + vestingSchedule.vestedEnd) {
+                _releaseAmount = _releaseAmount + vestingSchedule.releaseAmounts[i];
+                vestingSchedule.vestedEnd = vestingSchedule.vestedEnd +  vestingSchedule.durations[i];
+            }
+            else {
+                _startIndex = i;
+                break;
+            }
         }
-    }
-
-    function getCurrentTime()
-        internal
-        virtual
-        view
-        returns(uint256){
-        return block.timestamp;
+        return (_releaseAmount, _startIndex);
     }
 }
